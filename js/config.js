7d17e4143ddc2196a7ee399179df95c1b90c11a8
Justin Galicic <jgalicic@gmail.com>
2018-02-12 21:31:21 -0800

    Added project files

---
+++ js/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  consumer_key:         'PPgChdrWqoNj9dmrTTc5YKsLO',
+  consumer_secret:      'm2smQJ8D3y6DPPyfMnqpG1gb9u1jrFLWxdOf05kLLWzUXkumq0',
+  access_token:         '48225496-JLh3MqiLsB49o7dtZikYQkqzDjhgs8qoyFwt4phOj',
+  access_token_secret:  'V1ayEONEAEUSZWS0tEOelt0JPHtqbQopUwe4gKNiWelRv',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+}
+
+
+
+
